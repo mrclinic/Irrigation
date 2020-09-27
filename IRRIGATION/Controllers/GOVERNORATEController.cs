14111be892d35e9cfeb2532b9ce86fb936de7f5a
ذوالفقار علي ديب <zoalfkar_deb@hotmail.com>
2020-09-27 14:33:32 +0300

    add new project Irrigation

---
+++ IRRIGATION/Controllers/GOVERNORATEController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IRRIGATION.API;
+using IRRIGATIONDLL.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IRRIGATION.Controllers
+{
+    public class GOVERNORATEController : ApiControllerBase
+    {
+        public GOVERNORATEController(AppDBContext _db) : base(_db) { }
+         public IActionResult Index()
+        {
+            return View();
+        }
+           public ActionResult GOVERNORATE_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            try
+            {
+                IQueryable<GOVERNORATE> _GOVERNORATE = db.GOVERNORATE.AsQueryable();
+
+                DataSourceResult result = _GOVERNORATE.ToDataSourceResult(request, governorate => new
+                {
+                    Id = governorate.Id,
+                    CREATION_DATE = governorate.CREATION_DATE,
+                    CREATE_USERNB = governorate.CREATE_USERNB,
+                    UPDATE_DATE = governorate.UPDATE_DATE,
+                    UPDATE_USERNB = governorate.UPDATE_USERNB,
+                    NAME = governorate.NAME,
+                    ORDER = governorate.ORDER,
+                    NOTE = governorate.NOTE,
+
+                });
+                return Json(result);
+            }
+            catch (Exception e)
+            {
+                return Json(new { Data = new[] { String.Empty }, Total = "0", AggregateResults = "", Errors = new { msg = new { errors = new[] { e.Message } } } });
+            }
+        }
+           public ActionResult GOVERNORATE_Create([DataSourceRequest] DataSourceRequest request, GOVERNORATE governorate)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new GOVERNORATE
+                {
+                    Id = governorate.Id,
+                    CREATION_DATE = governorate.CREATION_DATE,
+                    CREATE_USERNB = governorate.CREATE_USERNB,
+                    UPDATE_DATE = governorate.UPDATE_DATE,
+                    UPDATE_USERNB = governorate.UPDATE_USERNB,
+                    NAME = governorate.NAME,
+                    ORDER = governorate.ORDER,
+                    NOTE = governorate.NOTE,
+
+                };
+                try
+                {
+                    db.GOVERNORATE.Add(entity);
+                    db.SaveChanges();
+                    governorate.Id = entity.Id;
+                }
+                catch (Exception e)
+                {
+                    return Json(new { Data = new[] { String.Empty }, Total = "0", AggregateResults = "", Errors = new { msg = new { errors = new[] { e.Message } } } });
+                }
+            }
+
+            return Json(new[] { governorate }.ToDataSourceResult(request, ModelState));
+        }
+           public ActionResult GOVERNORATE_Update([DataSourceRequest] DataSourceRequest request, GOVERNORATE governorate)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new GOVERNORATE
+                {
+                    Id = governorate.Id,
+                    CREATION_DATE = governorate.CREATION_DATE,
+                    CREATE_USERNB = governorate.CREATE_USERNB,
+                    UPDATE_DATE = governorate.UPDATE_DATE,
+                    UPDATE_USERNB = governorate.UPDATE_USERNB,
+                    NAME = governorate.NAME,
+                    ORDER = governorate.ORDER,
+                    NOTE = governorate.NOTE,
+
+                };
+                try
+                {
+                    db.GOVERNORATE.Attach(entity);
+                    db.Entry(entity).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    return Json(new { Data = new[] { String.Empty }, Total = "0", AggregateResults = "", Errors = new { msg = new { errors = new[] { e.Message } } } });
+                }
+            }
+
+            return Json(new[] { governorate }.ToDataSourceResult(request, ModelState));
+        }
+         public ActionResult GOVERNORATE_Destroy([DataSourceRequest] DataSourceRequest request, GOVERNORATE governorate)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new GOVERNORATE
+                {
+                    Id = governorate.Id,
+                    CREATION_DATE = governorate.CREATION_DATE,
+                    CREATE_USERNB = governorate.CREATE_USERNB,
+                    UPDATE_DATE = governorate.UPDATE_DATE,
+                    UPDATE_USERNB = governorate.UPDATE_USERNB,
+                    NAME = governorate.NAME,
+                    ORDER = governorate.ORDER,
+                    NOTE = governorate.NOTE,
+
+                };
+                try
+                {
+                    db.Remove(entity);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    return Json(new { Data = new[] { String.Empty }, Total = "0", AggregateResults = "", Errors = new { msg = new { errors = new[] { e.Message } } } });
+                }
+            }
+
+            return Json(new[] { governorate }.ToDataSourceResult(request, ModelState));
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ IRRIGATION/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IRRIGATIONDLL.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+
+namespace GridAndMenuCoreApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                // Maintain property names during serialization. See:
+                // https://github.com/aspnet/Announcements/issues/194
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            // Add Kendo UI services to the services container
+              services.AddKendo();
+//            services.AddDbContext<AppDBContext>(options =>
+//                options.UseOracle(
+//                    "User Id = IRRIGATION;Password=123;Data Source = localhost:1521/ORCL;"
+//                    , b =>
+//b.UseOracleSQLCompatibility("11")));
+            services.AddDbContext<AppDBContext>();
+            services.AddScoped<AppDBContext>();
+
+            services.TryAddTransient<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            
+        }
+    }
+}
